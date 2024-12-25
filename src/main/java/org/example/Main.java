1aa5ecd76356873a567d7d5f50764653ff0f10d8
AntonSharykin <antonazovec@gmail.com>
2024-12-25 19:32:33 +0100

    change print

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-       UserService.saveTodb(new User(38,"Anton"));
+       UserService.saveTodb(new User(38,"Anton2"));
         System.out.println("Hello world!");
         PrintService.print();
     }
